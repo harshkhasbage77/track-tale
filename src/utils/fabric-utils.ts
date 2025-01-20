923d8a8da00fca55573f45ead1674ed6281320b2
harshkhasbage77 <harshkhasbage77@gmail.com>
2025-01-20 19:24:05 +0530

    BugFix: Saturation effect made significantly visible

--- src/utils/fabric-utils.ts
+++ src/utils/fabric-utils.ts
@@ -184,7 +184,7 @@ function getFilterFromEffectType(effectType: EffecType){
         case "invert":
             return "invert(100%)";
         case "saturate":
-            return "saturate(100%)";
+            return "saturate(50%)";
         case "blueScreen":
             return "grayscale(100%) opacity(45%)";
             // return "opacity(50%) drop-shadow(0 0 10px rgba(0, 123, 255, 0.5))";
